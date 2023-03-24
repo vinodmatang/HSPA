fbfe7b787e35c3853c85760d41b3aff7fdca1716
vinodmatang <vinod.matang@gmail.com>
2023-03-24 20:06:52 +0500

    20230324

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,5 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { HousingService } from 'src/app/services/housing.service';
 import { IProperty } from '../IProperty.Interface';
 
@@ -9,14 +10,19 @@ import { IProperty } from '../IProperty.Interface';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
+  SellRent = 1;
   properties: Array<IProperty>;
-  constructor(private housingService:HousingService){ }
+  constructor(private route: ActivatedRoute, private housingService:HousingService){ }
 
   ngOnInit(): void {
-    this.housingService.getAllProperties().subscribe(
+    if (this.route.snapshot.url.toString()){
+      this.SellRent=2; //Mean we are on rent-property URL else we are on base URL
+    }
+    this.housingService.getAllProperties(this.SellRent).subscribe(
       data=> {
         this.properties=data;
         console.log(data);
+        console.log(this.route.snapshot.url.toString());
         }, error => {
           console.log(error);
         }
